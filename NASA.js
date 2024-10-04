cefe75f3241f91a2c7b71d17cf7eca7cb6a29c7d
arhaana <129735524+arhaana@users.noreply.github.com>
2024-10-04 22:20:02 +0530

    Add files via upload

---
+++ NASA.js
@@ -0,0 +1,4 @@
+// Add interactivity if desired
+document.querySelector('.cta-button').addEventListener('click', () => {
+    document.getElementById('explore').scrollIntoView({ behavior: 'smooth' });
+});
